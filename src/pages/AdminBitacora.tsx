9c999bded84735500121dd8572a0805eb5b745bf
Oerlinker <oerlinker@gmail.com>
2025-04-26 21:17:14 -0400

    Produccion 1.8  Bitacora

--- src/pages/AdminBitacora.tsx
+++ src/pages/AdminBitacora.tsx
@@ -1,3 +1,4 @@
+//
 import React, {useState, useEffect} from "react";
 import {useQuery} from "@tanstack/react-query";
 import API from "../services/api";
@@ -36,7 +37,6 @@ const AdminBitacora = () => {
     const [dateRange, setDateRange] = useState<DateRange | undefined>(undefined);
     const [bitacoras, setBitacoras] = useState<Bitacora[]>([]);
     const {toast} = useToast()
-    const [isCalendarOpen, setIsCalendarOpen] = useState(false);
 
     const {data, isLoading, error, refetch} = useQuery({
         queryKey: ["bitacoras", userId, dateRange],
@@ -119,73 +119,43 @@ const AdminBitacora = () => {
                         </div>
                         <div>
                             <Label>Rango de Fechas:</Label>
-                            <Button
-                                className={cn(
-                                    "w-[300px] justify-start text-left font-normal",
-                                    !dateRange?.from && "text-muted-foreground"
-                                )}
-                                variant={"outline"}
-                                onClick={() => setIsCalendarOpen(!isCalendarOpen)}
-                            >
-                                <CalendarIcon className="mr-2 h-4 w-4"/>
-                                {dateRange?.from ? (
-                                    dateRange.to ? (
-                                        `${format(dateRange.from, "LLL dd, y")} - ${format(
-                                            dateRange.to,
-                                            "LLL dd, y"
-                                        )}`
-                                    ) : (
-                                        format(dateRange.from, "LLL dd, y")
-                                    )
-                                ) : (
-                                    <span>Pick a date</span>
-                                )}
-                            </Button>
-                            {isCalendarOpen && (
-                                <div className="mt-2">
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <Button
+                                        variant={"outline"}
+                                        className={cn(
+                                            "w-[300px] justify-start text-left font-normal",
+                                            !dateRange?.from && "text-muted-foreground"
+                                        )}
+                                    >
+                                        <CalendarIcon className="mr-2 h-4 w-4"/>
+                                        {dateRange?.from ? (
+                                            dateRange.to ? (
+                                                `${format(dateRange.from, "LLL dd, y")} - ${format(
+                                                    dateRange.to,
+                                                    "LLL dd, y"
+                                                )}`
+                                            ) : (
+                                                format(dateRange.from, "LLL dd, y")
+                                            )
+                                        ) : (
+                                            <span>Pick a date</span>
+                                        )}
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-auto p-0" align="start">
                                     <Calendar
+                                        mode="range"
                                         defaultMonth={dateRange?.from}
+                                        selected={dateRange}
+                                        onSelect={setDateRange}
                                         disabled={(date) =>
                                             date > new Date() || date < new Date("2023-01-01")
                                         }
                                         initialFocus
-                                        mode="range"
-                                        onSelect={setDateRange}
-                                        selected={dateRange}
-                                        classNames={{
-                                            months: "flex flex-col sm:flex-row space-y-2 sm:space-x-2 sm:space-y-0",
-                                            month: "space-y-2",
-                                            caption: "flex justify-center pt-1 relative items-center",
-                                            caption_label: "text-sm font-medium",
-                                            nav: "space-x-1 flex items-center",
-                                            nav_button: cn(
-                                                "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
-                                            ),
-                                            nav_button_previous: "absolute left-1",
-                                            nav_button_next: "absolute right-1",
-                                            table: "w-full border-collapse space-y-1",
-                                            head_row: "flex",
-                                            head_cell:
-                                                "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
-                                            row: "flex w-full mt-2",
-                                            cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
-                                            day: cn(
-                                                "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
-                                            ),
-                                            day_range_end: "day-range-end",
-                                            day_selected:
-                                                "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-                                            day_today: "bg-accent text-accent-foreground",
-                                            day_outside:
-                                                "text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
-                                            day_disabled: "text-muted-foreground opacity-50",
-                                            day_range_middle:
-                                                "aria-selected:bg-accent aria-selected:text-accent-foreground",
-                                            day_hidden: "invisible",
-                                        }}
                                     />
-                                </div>
-                            )}
+                                </PopoverContent>
+                            </Popover>
                         </div>
                         <div className="flex items-end space-x-2">
                             <Button onClick={() => handleSearch()}>Buscar</Button>
